81f504262b41f94fa852458a73bf7818408b26bb
joonaramo <joona00ramo@gmail.com>
2022-02-10 15:18:31 +0200

    exercise 13.22

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,10 +1,26 @@
-const { Reading } = require('../models');
+const { Reading, User } = require('../models');
+const { tokenExtractor } = require('../util/tokenExtractor');
 
 const expressRouter = require('express').Router();
 const router = require('@root/async-router').wrap(expressRouter);
 
 router.post('/', async (req, res) => {
-  const readinglist = Reading.create(req.body);
+  const readinglist = await Reading.create(req.body);
+  res.json(readinglist);
+});
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+  const readinglist = await Reading.findByPk(req.params.id);
+  const user = await User.findByPk(req.decodedToken.id);
+  if (readinglist.userId !== user.id) {
+    let err = new Error();
+    err.name = 'NotAuthorizedError';
+    throw err;
+  }
+  if (req.body.read !== undefined) {
+    readinglist.read = req.body.read;
+  }
+  await readinglist.save();
   res.json(readinglist);
 });
 
