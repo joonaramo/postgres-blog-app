9d3d3d147a9a43f9a1f5d666907fe5d718738c32
joonaramo <joona00ramo@gmail.com>
2022-02-10 11:46:17 +0200

    exercise 13.19

--- models/index.js
+++ models/index.js
@@ -1,8 +1,12 @@
 const Blog = require('./Blog');
+const Reading = require('./Reading');
 const User = require('./User');
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
+
+User.belongsToMany(Blog, { through: Reading });
+Blog.belongsToMany(User, { through: Reading });
 
 module.exports = {
   Blog,
